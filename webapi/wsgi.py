a75a1239dac34a0a1357d2fdf3f6ed4106ae135b
adrianliaw <adrianliaw2000@gmail.com>
2017-06-06 17:11:32 +0800

    Download tickets and sources at app startup

--- webapi/wsgi.py
+++ webapi/wsgi.py
@@ -1,8 +1,20 @@
+import os
+import subprocess
+from urllib.request import urlretrieve
 from django.core.wsgi import get_wsgi_application
 from werkzeug.wsgi import DispatcherMiddleware
 
 from .app import app as flask_app
 from .line.app import configure_linebot_app
+
+
+subprocess.call(['python', '-m', 'quizzler.migrations'])
+
+if not os.path.isfile('sources.zip'):
+    urlretrieve(os.environ.get('SOURCES_URL'), filename='sources.zip')
+
+if not os.path.isfile('tickets.zip'):
+    urlretrieve(os.environ.get('TICKETS_URL'), filename='tickets.zip')
 
 
 configure_linebot_app(flask_app)
