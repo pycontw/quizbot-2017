69aa372d049fda656f1017849d8ac0490c49529f
Tzu-ping Chung <uranusjr@gmail.com>
2017-06-11 13:37:35 +0800

    Disable the bots

--- webapi/wsgi.py
+++ webapi/wsgi.py
@@ -1,12 +1,12 @@
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 from werkzeug.wsgi import DispatcherMiddleware
 
 from .app import app as flask_app
-from .line.app import configure_linebot_app
+# from .line.app import configure_linebot_app
 
 
-configure_linebot_app(flask_app)
+# configure_linebot_app(flask_app)
 
 app = DispatcherMiddleware(flask_app, {
-    '/api/fb_webhook': get_wsgi_application(),
+    # '/api/fb_webhook': get_wsgi_application(),
 })
